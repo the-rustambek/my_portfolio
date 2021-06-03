669b990265227953a1e7e8206d425fddd71640aa
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-06-03 07:18:12 -0700

    add file

--- main.js
+++ main.js
@@ -16,11 +16,9 @@ const closeButtonElement = document.querySelector('.close-button')
 
 
 openButtonElement.addEventListener("click", event => {
-     overlayElement.classList.add('nav-overlay')
      overlayElement.classList.remove('d-none')
 })
 
 closeButtonElement.addEventListener("click", event => {
-    overlayElement.classList.remove('nav-overlay')
     overlayElement.classList.add('d-none')
 })
