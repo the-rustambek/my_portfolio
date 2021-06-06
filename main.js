27d1561511a4c3c24c2c6f41a10188d3ab72d82b
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-06-06 07:05:11 -0700

    add file

--- main.js
+++ main.js
@@ -2,10 +2,21 @@ const overlayElement = document.querySelector('#overlay');
 const openButtonElement = document.querySelector('.bar-button ')
 const closeButtonElement = document.querySelector('.close-button')
 
+const navbarTitleElement= document.querySelector(".navbar-title")
+const navbarToggleElement=document.querySelector(".bar-button")
+const avatarImgElement=document.querySelector(".wrapper-avatar")
+const wrapperElement=document.querySelector(".wrapper")
+const wrapperInfoElement=document.querySelector(".wrapper-info")
 
 openButtonElement.addEventListener("click", event => {
      overlayElement.classList.add('nav-overlay')
      overlayElement.classList.remove('d-none')
+     avatarImgElement.classList.add("d-none")
+     navbarToggleElement.classList.add("d-none")
+     navbarTitleElement.classList.add("d-none")
+     wrapperElement.style.backgroundImage="none"
+     
+     wrapperInfoElement.classList.add("d-none")
 
 })
 
@@ -13,4 +24,11 @@ openButtonElement.addEventListener("click", event => {
 closeButtonElement.addEventListener("click", event => {
     overlayElement.classList.remove('nav-overlay')
     overlayElement.classList.add('d-none')
+    avatarImgElement.classList.remove("d-none")
+    
+    navbarToggleElement.classList.remove("d-none")
+    navbarTitleElement.classList.remove("d-none")
+    
+    
+    wrapperInfoElement.classList.remove("d-none")
 })
